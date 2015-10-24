1540f360fdabcc0878a62813c558ee772f6eb830
Jakob Pedersen <jakob@actioncaller.com>
2015-10-24 22:17:35 +0200

    Working DHT22 and kind of working MQ135
    The MQ135 gives the CO2 PPM as single digit numbers, but it seems to react to breathing on it - it is probably some kind of configuration problem.

--- Arduino_CO2_sensor/Arduino_CO2_sensor.ino
+++ Arduino_CO2_sensor/Arduino_CO2_sensor.ino
@@ -9,6 +9,12 @@
 
 #include <MQ135.h>
 #include <RunningAverage.h>
+#include "DHT.h"
+
+//DHT22 config
+#define DHTTYPE DHT22   // Senor type: DHT 22  (AM2302), AM2321
+#define DHTPIN 2     // Connected to digital PIN 2
+DHT dht(DHTPIN, DHTTYPE); // Initialize sensor
 
 //MQ135.h config
 MQ135 gasSensor = MQ135(0);
@@ -23,11 +29,15 @@ int samples = 0;
 void setup() {
   Serial.begin(9600);      // sets the serial port to 9600
 
+  dht.begin(); // Not sure what this does
+  
   runningAverage.clear(); // explicitly start RunningAverageclean
 }
 
 void loop() {
 
+
+/*============ BEGIN MQ135 ===================*/
   // Get and echo CO2 PPM
   float ppm = gasSensor.getPPM();
   Serial.print("Current PPM reading: ");
@@ -50,6 +60,48 @@ void loop() {
     runningAverage.clear();
     Serial.print("RESET Running Average");
   }
+/*============ END MQ135 ===================*/
+
+/*============ BEGIN DHT22 ===================*/
+// Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+
+/*============ END DHT22 ===================*/
+
 
   delay(1000);
 }
+
+
+
+
